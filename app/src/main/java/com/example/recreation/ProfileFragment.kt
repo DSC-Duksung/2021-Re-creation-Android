6ba62799472eef1b587cda1e6c6f07ba5e90fc7f
ariha1982 <71639371+ariha1982@users.noreply.github.com>
2021-03-04 14:54:20 +0900

    hj: 구글소셜로그인 연동(진행중), 화면전환

--- app/src/main/java/com/example/recreation/ProfileFragment.kt
+++ app/src/main/java/com/example/recreation/ProfileFragment.kt
@@ -1,18 +1,121 @@
 package com.example.recreation
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_profile.*
 
-class ProfileFragment : Fragment() {
-
+class ProfileFragment : Fragment(), View.OnClickListener {
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bnt_profile.setOnClickListener {
+            activity?.let {
+                val intent = Intent(activity, ProfileContents::class.java)
+                startActivity(intent)
+
+            }
+        }
+    }
+
+
+    //google client
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var firebaseAuth: FirebaseAuth
+    private val RC_SIGN_IN = 99
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //btn_googleSignIn.setOnClickListener (this) // 구글 로그인 버튼
+        btn_login
+            .setOnClickListener {signIn()}
+
+        //Google 로그인 옵션 구성. requestIdToken 및 Email 요청
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.firebase_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        firebaseAuth = Firebase.auth
+    }
+
+    // onStart. 유저가 앱에 이미 구글 로그인을 했는지 확인
+    override fun onStart() {
+        super.onStart()
+        val currentUser = firebaseAuth.currentUser
+
+    } //onStart End
+
+    // onActivityResult
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d("LoginActivity", "firebaseAuthWithGoogle:"+account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("LoginActivity", "Google sign in failed", e)
+            }
+        }
+    } // onActivityResult End
+
+    // firebaseAuthWithGoogle
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("LoginActivity", "firebaseAuthWithGoogle 성공")
+                } else {
+                    Log.w("LoginActivity", "firebaseAuthWithGoogle 실패", task.exception)
+                }
+            }
+    }// firebaseAuthWithGoogle END
+
+
+    // toMainActivity
+    fun toMainActivity(user: FirebaseUser?) {
+        if(user !=null) { // MainActivity 로 이동
+            val intent = Intent(activity, MainActivity::class.java)
+            startActivity(intent)
+        }
+    } // toMainActivity End
+
+    // signIn
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+    // signIn End
+
+    override fun onClick(p0: View?) {
+    }
+
 }
