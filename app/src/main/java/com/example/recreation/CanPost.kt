4afaf42c83cc7e8f0f61e5586243c97cdf4b692f
ariha1982 <71639371+ariha1982@users.noreply.github.com>
2021-03-08 00:34:08 +0900

    hj: 각 용기별 게시판

---
+++ app/src/main/java/com/example/recreation/CanPost.kt
@@ -0,0 +1,12 @@
+package com.example.recreation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CanPost : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.post_can)
+    }
+}
