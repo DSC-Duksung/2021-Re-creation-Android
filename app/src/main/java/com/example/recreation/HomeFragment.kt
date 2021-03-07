0647bfffaa74855da203d4eb6d3eba5f05cac797
sss20young <ksyt98@gmail.com>
2021-03-07 23:11:39 +0900

    sy: home에서 로그인 되어있을 시 name 변경

--- app/src/main/java/com/example/recreation/HomeFragment.kt
+++ app/src/main/java/com/example/recreation/HomeFragment.kt
@@ -1,15 +1,19 @@
 package com.example.recreation
 
+import android.content.Context
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_home.*
 
-
 class HomeFragment : Fragment() {
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -19,8 +23,19 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        firebaseAuth = Firebase.auth
+    }
+
     override fun onStart() {
         super.onStart()
+
+        val currentUser = firebaseAuth.currentUser
+        if ( currentUser != null ) { // 로그인 했다면
+            displayName.setText(currentUser.displayName)
+        }
 
         var animation: AnimationDrawable = imageView.background as AnimationDrawable
         animation.start()
