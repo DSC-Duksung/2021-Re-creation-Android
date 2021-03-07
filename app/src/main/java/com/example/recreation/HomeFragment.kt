4a1bbacb492c40023f1b3f0bdce955615a315a1a
sss20young <ksyt98@gmail.com>
2021-03-07 22:33:45 +0900

    sy: home animation

--- app/src/main/java/com/example/recreation/HomeFragment.kt
+++ app/src/main/java/com/example/recreation/HomeFragment.kt
@@ -1,20 +1,13 @@
 package com.example.recreation
 
-import android.app.Activity
-import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import com.example.recreation.model.ImageClassifier
-import com.example.recreation.model.Keys
-import io.reactivex.rxjava3.kotlin.subscribeBy
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_home.*
-import java.io.FileNotFoundException
+
 
 class HomeFragment : Fragment() {
 
@@ -24,5 +17,13 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        var animation: AnimationDrawable = imageView.background as AnimationDrawable
+        animation.start()
+        //imageView.setImageDrawable(resources.getDrawable(R.drawable.heather2))
     }
 }
